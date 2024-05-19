c36d7a01b9c0ce254330e476481ae2163bed87cf
AditiMishra2003 <115421723+AditiMishra2003@users.noreply.github.com>
2024-05-20 00:21:45 +0530

    Updated Solution - GfG to GitHub

--- Medium/Find Missing And Repeating/find-missing-and-repeating.cpp
+++ Medium/Find Missing And Repeating/find-missing-and-repeating.cpp
@@ -8,24 +8,25 @@ class Solution{
 public:
     vector<int> findTwoElement(vector<int> arr, int n) {
         // code here
-        long long len=arr.size();
         vector<int> ans;
-        long long S1=(len*(len+1))/2;
-        long long S2=(len*(len+1)*(2*len+1))/6;
-        long long s1=0;
-        long s2=0;
+        int len=arr.size();
+        int n1=n+1;
+        vector<int> v(n1,0);
         for(int i=0;i<len;i++){
-            s1+=arr[i];
-            s2+=(long long)arr[i]*(long long)arr[i];
+            v[arr[i]]++;
         }
-        long long val1=s1-S1;
-        long long val2=s2-S2;
-        val2=val2/val1;
-        long long missing=(val1+val2)/2;
-        long long repeating=missing-val1;
-        ans.push_back(missing);
-        ans.push_back(repeating);
+        int missing=-1;
+        int repeating=-1;
+        for(int i=1;i<=n1;i++){
+            if(v[i]==0)missing=i;
+            else if(v[i]==2)repeating=i;
+            if(repeating!=-1&&missing!=-1){
+                break;
+            }
+        }
         
+        ans.push_back(repeating);
+        ans.push_back(missing);
         return ans;
     }
 };
